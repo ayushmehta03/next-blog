0263b5516d4cfcafb65ca3b40dbe76131348b51f
Ayush Mehta <ayushmehtabps@gmail.com>
2025-06-22 19:34:47 +0530

    blog-schema implemented

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,7 +1,39 @@
+import handleSumbission from "@/app/action"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+
 export default function CreateBlogPost(){
-    return(
-        <div>
-            <h1>Create Posts</h1>
-        </div>
+    return(<div>
+        <Card className="max-w-lg mx-auto py-8">
+            <CardHeader>
+            <CardTitle>Create Post</CardTitle> 
+            <CardDescription>Create a new post to share with the world</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form className="flex flex-col gap-4" action={handleSumbission}>
+                    <div className="flex flex-col gap-2">
+                        <Label>Title</Label>
+                        <Input name="title" required type="text" placeholder="Title" />
+                    </div>
+
+
+                     <div className="flex flex-col gap-2">
+                        <Label>Content</Label>
+                        <Textarea  name="content" required placeholder="Content" />
+                            </div>
+
+                         <div className="flex flex-col gap-2">
+                        <Label>Image Url</Label>
+                        <Input  name="url" required type="url" placeholder="ImageUrl"/>
+                    </div>
+
+                    <Button>Create Post</Button>
+                </form>
+            </CardContent>
+        </Card>
+    </div>
     )
 }
